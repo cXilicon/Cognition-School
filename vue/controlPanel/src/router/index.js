d32fe861edf71d77e9e9587cf8c4cef6bdb5c058
Hsaeno <47032254+Hsaeno@users.noreply.github.com>
2020-12-01 12:54:34 +0800

    页面跳转

--- vue/controlPanel/src/router/index.js
+++ vue/controlPanel/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '../pages/HelloWorld'
+import Login from '../pages/login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+          component:resolve => require(['../pages/login.vue'],resolve),
+          meta: { title: '登录' }
+    },
+      {
+        path: '/helloWorld',
+        component:resolve => require(['../pages/HelloWorld.vue'],resolve),
+        meta: { title: '登录' }
     }
+    ,
   ]
 })
