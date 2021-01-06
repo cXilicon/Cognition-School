d66305dde7dd99414ca4a1c4d0b7f20f2111b65f
Hsaeno <47032254+Hsaeno@users.noreply.github.com>
2021-01-06 21:03:47 +0800

    delete

--- SpringBoot/back/src/main/java/com/ash/softwareback/Service/TblAdminService.java
+++
@@ -1,9 +0,0 @@
-package com.ash.softwareback.Service;
-
-
-import com.ash.softwareback.entity.AdminEntity;
-
-public interface TblAdminService {
-
-    AdminEntity loginAdmin(String adminName, String password);
-}
